0b09aa965916d2cf4778435ac178d34637239c81
Herberton <herberton.dev@gmail.com>
2024-01-29 15:17:51 -0400

    trabalhando add prof ao curso, listando prof do curso.

--- src/main/java/br/school/dto/ListaProfessorCursoDTO.java
+++ src/main/java/br/school/dto/ListaProfessorCursoDTO.java
@@ -4,7 +4,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 public class ListaProfessorCursoDTO {
     private Long id;
-    @Schema(example = "Gustavo Lemos", required = true)
+    @Schema(example = "Gustavo Lemos ", required = true)
     private String nome;
 
     private CursoDTO curso;
